44c5a062ca302e414bbadfe76bc842ad6cad0eff
Tim Zimmermann <tim@linux4.de>
2022-08-26 14:29:14 +0200

    selene: Initial health AIDL implementation
    * Granted correct permissions on fb0 by default
    * Enabled suspend according to
      https://android.googlesource.com/platform/hardware/interfaces/+/refs/tags/android-13.0.0_r3/health/aidl#charger-enable-suspend
    
    Change-Id: I41b84840a03a57d46beb1a7520182de2bb29d2f5
    Signed-off-by: Sushrut1101 <guptasushrut@gmail.com>

--- device.mk
+++ device.mk
@@ -234,9 +234,8 @@ PRODUCT_PACKAGES += \
 
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.1-impl \
-    android.hardware.health@2.1-impl.recovery \
-    android.hardware.health@2.1-service
+    android.hardware.health-service.selene \
+    android.hardware.health-service.selene-recovery
 
 # IMS
 PRODUCT_BOOT_JARS += \
