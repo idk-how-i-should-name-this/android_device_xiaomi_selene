ad83fc1b47d6e1ea13f2318fe6a1f65fc0dce4c0
Tim Zimmermann <tim@linux4.de>
2022-12-04 10:59:51 +0100

    selene: Adapt for leaf
    [fukiame: "the Leaf way": dont override fp]
    
    Change-Id: Idd21993e45ad087a54195b78f2972fe795a3a3e1

--- selene.mk
+++ selene.mk
@@ -16,20 +16,11 @@ $(call inherit-product, vendor/leaf/config/common_full_phone.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := selene
-PRODUCT_NAME := leaf_selene
+PRODUCT_NAME := selene
 PRODUCT_BRAND := Redmi
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := Redmi 10
-PRODUCT_SYSTEM_NAME := selene
-PRODUCT_SYSTEM_DEVICE := selene
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 TARGET_VENDOR := xiaomi
 TARGET_VENDOR_PRODUCT_NAME := selene
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="selene-user 12 TP1A.220624.014 V14.0.4.0.TKUMIXM release-keys" \
-    TARGET_DEVICE=$(PRODUCT_SYSTEM_DEVICE) \
-    TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME)
-
-# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := "Redmi/selene/selene:12/TP1A.220624.014/V14.0.4.0.TKUMIXM:user/release-keys"
