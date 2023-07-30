cb58f78bebf6a1aa99504bb645dd5764afb5abef
fukiame <fukiame@proton.me>
2023-07-30 18:44:15 +0700

    selene: makefile: refactor
    ...looks messy and doesnt follow typical conventions
    
    * switch to SPDX license identifier
    * move device inheritance and boot anim flag down
    * stop importing aosp base as its redundant
    * remove redundant flag
    
    also drop lineage_selene.mk

--- arrow_selene.mk
+++ arrow_selene.mk
@@ -1,33 +1,14 @@
 #
 # Copyright (C) 2021 The LineageOS Project
 #
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
+# SPDX-License-Identifier: Apache-2.0
 #
-# http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-
-# Boot animation
-TARGET_SCREEN_WIDTH := 1080
-TARGET_SCREEN_HEIGHT := 2400
-
-# Inherit from selene device makefile
-$(call inherit-product, device/xiaomi/selene/device.mk)
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
-# Inherit from our custom product configuration
-TARGET_DISABLE_EPPE := true
+# Inherit some common Arrow stuff
 $(call inherit-product, vendor/arrow/config/common.mk)
 
 #
@@ -35,6 +16,13 @@ $(call inherit-product, vendor/arrow/config/common.mk)
 #
 $(call inherit-product, $(SRC_TARGET_DIR)/product/handheld_system_ext.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_system_ext.mk)
+
+# Inherit from selene device makefile
+$(call inherit-product, device/xiaomi/selene/device.mk)
+
+# Boot animation
+TARGET_SCREEN_WIDTH := 1080
+TARGET_SCREEN_HEIGHT := 2400
 
 # Some specific flags
 TARGET_BOOT_ANIMATION_RES := 1080
