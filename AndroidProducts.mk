81f9d535c0f172b08077558672dc847ba921a0e4
fukiame <fukiame@proton.me>
2023-10-01 07:18:13 +0700

    selene: leafos buildbot mk2

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-   $(LOCAL_DIR)/lineage_selene.mk
+   $(LOCAL_DIR)/leaf_selene.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_selene-user \
-    lineage_selene-userdebug \
-    lineage_selene-eng
+    leaf_selene-user \
+    leaf_selene-userdebug \
+    leaf_selene-eng
--- leaf_selene.mk
+++ leaf_selene.mk
@@ -8,17 +8,11 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common SkylineUI stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
-
-#
-# All components inherited here go to system_ext image
-#
-$(call inherit-product, $(SRC_TARGET_DIR)/product/handheld_system_ext.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/telephony_system_ext.mk)
-
-# Inherit from selene device makefile
+# Inherit from device makefile
 $(call inherit-product, device/xiaomi/selene/device.mk)
+
+# Inherit common LeafOS stuff.
+$(call inherit-product, vendor/leaf/config/common_full_phone.mk)
 
 # Boot animation
 TARGET_SCREEN_WIDTH := 1080
@@ -26,7 +20,7 @@ TARGET_SCREEN_HEIGHT := 2400
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := selene
-PRODUCT_NAME := lineage_selene
+PRODUCT_NAME := leaf_selene
 PRODUCT_BRAND := Redmi
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := Redmi 10
