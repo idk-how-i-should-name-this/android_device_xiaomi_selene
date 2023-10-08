d1f6fbb07edce446bc7c359767b0da90f0363093
Beru Krugermeier <windowz414@gnuweeb.org>
2023-10-08 13:00:46 +0000

    selene: Switch to WiFi AIDL
    Signed-off-by: Beru Krugermeier <windowz414@gnuweeb.org>

--- device.mk
+++ device.mk
@@ -601,7 +601,7 @@ PRODUCT_PACKAGES += \
     libkeystore-engine-wifi-hidl
 
 PRODUCT_PACKAGES += \
-    android.hardware.wifi@1.0-service-lazy.selene
+    android.hardware.wifi-service
 
 PRODUCT_PACKAGES += \
     hpstapd \
