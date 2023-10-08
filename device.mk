a5d095800fa7a5a19012a2150eb079adbad1b2ae
bengris32 <bengris32@protonmail.ch>
2023-10-08 18:16:54 +0000

    selene: Switch to AOSP hostapd service
    Signed-off-by: Orkun Ergün <orkun68100@gmail.com>

--- device.mk
+++ device.mk
@@ -605,6 +605,7 @@ PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service-lazy.selene
 
 PRODUCT_PACKAGES += \
+    hpstapd \
     android.hardware.wifi@1.0 \
     android.hardware.wifi@1.0.vendor \
     android.hardware.wifi@1.1 \
@@ -615,12 +616,6 @@ PRODUCT_PACKAGES += \
     android.hardware.wifi@1.3.vendor \
     android.hardware.wifi@1.4 \
     android.hardware.wifi@1.4.vendor \
-    android.hardware.wifi.supplicant@1.0 \
-    android.hardware.wifi.supplicant@1.0.vendor \
-    android.hardware.wifi.supplicant@1.1 \
-    android.hardware.wifi.supplicant@1.1.vendor \
-    android.hardware.wifi.supplicant@1.2 \
-    android.hardware.wifi.supplicant@1.2.vendor \
     android.hardware.wifi.supplicant@1.3 \
     android.hardware.wifi.supplicant@1.3.vendor \
     android.hardware.wifi.hostapd@1.0 \
