e8f92e4ae21e3a7716312d77f62cf898ba77aaec
01soni247 <01soni247@gmail.com>
2023-11-06 18:43:43 +0700

    selene: Build android.hardware.wifi.supplicant@1.4
    * Fixes :- Cannot find entry android.hardware.wifi.supplicant@1.4::ISupplicant/default in either framework or device VINTF manifest.

--- device.mk
+++ device.mk
@@ -622,6 +622,7 @@ PRODUCT_PACKAGES += \
     android.hardware.wifi@1.4.vendor \
     android.hardware.wifi.supplicant@1.3 \
     android.hardware.wifi.supplicant@1.3.vendor \
+    android.hardware.wifi.supplicant@1.4.vendor \
     android.hardware.wifi.hostapd@1.0 \
     android.hardware.wifi.hostapd@1.0.vendor \
     android.hardware.wifi.hostapd@1.1 \
