0a2912f310cc77f1d610782ae9a76c6f351d85d1
fukiame <fukiame@proton.me>
2023-12-06 07:16:58 +0700

    selene: kenvyra bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-   $(LOCAL_DIR)/lineage_selene.mk
+   $(LOCAL_DIR)/kenvyra_selene.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_selene-user \
-    lineage_selene-userdebug \
-    lineage_selene-eng
+    kenvyra_selene-user \
+    kenvyra_selene-userdebug \
+    kenvyra_selene-eng
