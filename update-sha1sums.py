1f1dee8d908d020a52f6b0099bba413f0d0d3f50
fukiame <fukiame@proton.me>
2023-12-29 21:35:16 +0700

    selene: update-sha1sum: fixup device name and vendor
    i retartded

--- update-sha1sums.py
+++ update-sha1sums.py
@@ -20,8 +20,8 @@ import os
 import sys
 from hashlib import sha1
 
-device='msm8996-common'
-vendor='leeco'
+device='selene'
+vendor='xiaomi'
 
 with open('proprietary-files.txt', 'r') as f:
     lines = f.read().splitlines()
