aaf3a569d1782f25b599af81734f17675b2030e1
fukiame <fukiame@proton.me>
2023-12-29 16:58:59 +0700

    Revert "selene: keylayout: Add support for 3-button headset"
    This reverts commit ca73395f4bfaa6dbcc4a1aa9d98ac98a219f6c82.

--- device.mk
+++ device.mk
@@ -505,8 +505,7 @@ PRODUCT_COPY_FILES += \
 # Keylayout
 PRODUCT_COPY_FILES += \
     $(DEVICE_PATH)/configs/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/uinput-fpc.kl \
-    $(DEVICE_PATH)/configs/keylayout/uinput-goodix.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/uinput-goodix.kl \
-    $(DEVICE_PATH)/configs/keylayout/Generic.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/Generic.kl
+    $(DEVICE_PATH)/configs/keylayout/uinput-goodix.kl:$(TARGET_COPY_OUT_SYSTEM)/usr/keylayout/uinput-goodix.kl
 
 # HIDL
 PRODUCT_PACKAGES += \
