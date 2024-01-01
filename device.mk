ae07a3385a5e14be0ab1c11b035920531491f30a
fukiame <fukiame@proton.me>
2024-01-01 13:54:34 +0700

    Reapply "selene: Patch android.hardware.thermal@2.0-service.mtk to load VNDK 32 libhidlbase"
    This reverts commit 00e9313ca24f7406e5068df824b2d06db58456f0.

--- device.mk
+++ device.mk
@@ -587,7 +587,9 @@ PRODUCT_PACKAGES += \
 # VNDK 30 Files
 PRODUCT_COPY_FILES += \
     prebuilts/vndk/v30/arm64/arch-arm-armv8-a/shared/vndk-core/libmedia_helper.so:$(TARGET_COPY_OUT_VENDOR)/lib/libmedia_helper-v30.so \
-    prebuilts/vndk/v30/arm64/arch-arm64-armv8-a/shared/vndk-core/libmedia_helper.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libmedia_helper-v30.so
+    prebuilts/vndk/v30/arm64/arch-arm64-armv8-a/shared/vndk-core/libmedia_helper.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libmedia_helper-v30.so \
+    prebuilts/vndk/v32/arm64/arch-arm-armv8-a/shared/vndk-sp/libhidlbase.so:$(TARGET_COPY_OUT_VENDOR)/lib/libhidlbase-v32.so \
+    prebuilts/vndk/v32/arm64/arch-arm64-armv8-a/shared/vndk-sp/libhidlbase.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libhidlbase-v32.so
 
 # WiFi
 PRODUCT_COPY_FILES += \
