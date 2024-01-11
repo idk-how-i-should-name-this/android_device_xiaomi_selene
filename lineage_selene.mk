654a5ad55faa07fc1e9dc70a9e50ab1e890a46b4
fukiame <fukiame@proton.me>
2024-01-11 14:11:10 +0700

    selene: matrixx buildbot

--- lineage_selene.mk
+++ lineage_selene.mk
@@ -19,6 +19,12 @@ TARGET_SCREEN_WIDTH := 1080
 TARGET_SCREEN_HEIGHT := 2400
 TARGET_BOOT_ANIMATION_RES := 1080
 
+# Project Matrixx flags
+MATRIXX_MAINTAINER := fukiame
+MATRIXX_BATTERY := 5000mah
+MATRIXX_DISPLAY := 1080X2400
+MATRIXX_CHIPSET := MT6768
+
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := selene
 PRODUCT_NAME := lineage_selene
