0af394165b22f5becb7e2ce129866e04723a3c56
fukiame <fukiame@proton.me>
2024-01-26 19:05:16 +0700

    selene: exTHmUI bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-   $(LOCAL_DIR)/lineage_selene.mk
+   $(LOCAL_DIR)/exthm_selene.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_selene-user \
-    lineage_selene-userdebug \
-    lineage_selene-eng
+    exthm_selene-user \
+    exthm_selene-userdebug \
+    exthm_selene-eng
