f34d9e6c739f4ba288bde44f179bcabe3ff36509
fukiame <fukiame@proton.me>
2024-03-10 10:55:29 +0700

    [DNM][TMP]Revert "selene: Drop hard-coded fingerprint"
    play protect dying
    
    This reverts commit 00647e223b6e34015699636da1f406ce87315803.

--- miku_selene.mk
+++ miku_selene.mk
@@ -35,5 +35,9 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 TARGET_VENDOR := xiaomi
 TARGET_VENDOR_PRODUCT_NAME := selene
 PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="selene-user 12 TP1A.220624.014 V14.0.4.0.TKUMIXM release-keys" \
     TARGET_DEVICE=$(PRODUCT_SYSTEM_DEVICE) \
     TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME)
+
+# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
+BUILD_FINGERPRINT := "Redmi/selene/selene:12/TP1A.220624.014/V14.0.4.0.TKUMIXM:user/release-keys"
