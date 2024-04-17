086e815c7d77c24e790123573ad35b35faa9e76c
fukiame <fukiame@proton.me>
2024-04-17 13:49:24 +0700

    [SQUASH] selene: drop proprietary WiFi HALs remnants
    selene: proprietary-files: drop ipsec
    
    unneeded
    
    selene: drop wlan_assistant
    
    unneeded
    
    selene: drop mtk vendor wifi.supplicant
    
    with f9e4aec switching our stack to oss, this seems redundant
    
    Signed-off-by: fukiame <fukiame@proton.me>

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -97,14 +97,6 @@ LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.wlan_drv.rc
-LOCAL_MODULE_TAGS  := optional
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.wlan_drv.rc
-LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE       := meta_init.connectivity.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
